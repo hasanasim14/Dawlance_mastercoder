aa5981f1e9d59170d49cb2d2e6320c49e9cdbfd4
Hasan Aasim <hasan.asim14@gmail.com>
2025-07-04 16:23:29 +0500

    Changed the layout changes

--- src/app/(navbar-app)/layout.tsx
+++ src/app/(navbar-app)/layout.tsx
@@ -1,6 +1,6 @@
 import { AppSidebar } from "@/components/AppSidebar";
+import { Navbar } from "@/components/Navbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import Image from "next/image";
 
 export default function AppLayout({
   children,
@@ -12,30 +12,7 @@ export default function AppLayout({
       <div className="flex flex-1">
         <AppSidebar />
         <main className="flex-1 overflow-auto">
-          <div className="flex justify-between items-center p-4 sticky top-0 z-10 bg-background border-b">
-            {/* Top left image */}
-            <div className="relative h-10 w-32">
-              <Image
-                src="/logo.png"
-                alt="Dawlance logo"
-                fill
-                className="object-contain object-left"
-                priority
-              />
-            </div>
-
-            {/* Top right image */}
-            <div className="relative h-10 w-32">
-              <Image
-                src="aiSystemslogo.svg"
-                alt="Right header logo"
-                fill
-                className="object-contain object-right"
-                priority
-              />
-            </div>
-          </div>
-
+          <Navbar />
           <div className="p-2">{children}</div>
         </main>
       </div>
